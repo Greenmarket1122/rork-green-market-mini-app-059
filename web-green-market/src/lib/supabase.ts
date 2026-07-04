3be82efa448839477d634529b3c27e46a1600455
Greenmarket1122 <kurort289@gmail.com>
2026-07-04 13:07:21 +0500

    Update supabase.ts

--- web-green-market/src/lib/supabase.ts
+++ web-green-market/src/lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl =
+  "https://wucxfpinzsqfsnkvtdcr.supabase.co";
+
+const supabaseAnonKey =
+  "sb_publishable_EMgnn_QsIxjyULvTKKUCxQ_d9KAWifE";
+
 export const supabase = createClient(
-  "https://wucxfpinzsqfsnkvtdcr.supabase.co",
-  "sb_publishable_EMgnn_QsIxjyULvTKKUCxQ_d9KAWifE"
+  supabaseUrl,
+  supabaseAnonKey,
 );
