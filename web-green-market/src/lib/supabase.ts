9db70ff8056f33218f188dfe3dc19e4316bc99f0
Greenmarket1122 <kurort289@gmail.com>
2026-07-04 13:33:34 +0500

    Update supabase.ts

--- web-green-market/src/lib/supabase.ts
+++ web-green-market/src/lib/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-export function getSupabase(env: { SUPABASE_SERVICE_KEY: string }) {
+export function getSupabase(env: {
+  SUPABASE_SERVICE_KEY: string;
+}) {
   return createClient(
     "https://wucxfpinzsqfsnkvtdcr.supabase.co",
-    env.SUPABASE_SERVICE_KEY,
+    env.SUPABASE_SERVICE_KEY
   );
 }
